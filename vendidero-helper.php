c999ab7638a06e21971ca5fb80c361bbb65d2008
vendidero <dennis.nissle@iwebix.de>
2015-07-02 09:48:53 +0200

    1.0.2

--- vendidero-helper.php
+++ vendidero-helper.php
@@ -3,7 +3,7 @@
 Plugin Name: Vendidero Helper
 Plugin URI: http://vendidero.de
 Description: Will help vendidero users to manage their licenses and receive automatic updates
-Version: 1.0.1
+Version: 1.0.2
 Author: Vendidero
 Author URI: http://vendidero.de
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -20,7 +20,7 @@ final class Vendidero_Helper {
      */
     protected static $_instance = null;
 
-    public $version = '1.0.1';
+    public $version = '1.0.2';
 
     private $token = 'vendidero-api';
     private $api_url = 'https://vendidero.de/vd-api/';
